b76dcd68609b14cb0cc5333cf89fb3f43227c30a
Victor Wright <victor.wright@outlook.de>
2022-01-13 18:47:42 +0100

    button click

--- src/App.js
+++ src/App.js
@@ -127,11 +127,12 @@ function App() {
                 path="/"
                 exact
                 element={
-                  <Sharing
-                    // loading={loading}
-                    // categories={categories}
-                    APPDATA={APPDATA}
-                  />
+                  <About APPDATA={APPDATA} />
+                  // <Sharing
+                  //   // loading={loading}
+                  //   // categories={categories}
+                  //   APPDATA={APPDATA}
+                  // />
                 }
               />
               <Route
--- src/content/pages/About.js
+++ src/content/pages/About.js
@@ -28,23 +28,23 @@ const About = ({ APPDATA }) => {
       <div className="page-title">
         <h2>
           {/* <Firework/> */}
-          <span onDragEnd={handleClick}>-·≡ {APPDATA.INFO} ≡·-</span>
+          <span onDragEnd={handleClick}>-‧≡ {APPDATA.INFO} ≡‧-</span>
         </h2>
       </div>
 
       <div className="cont1">
-        <div className="aboutimg1">
-          <h1 className=" aboutt">
-            <Link to={"/sharing"}>Sharing</Link>
-          </h1>
-          <div className="color-overlay"></div>
-        </div>
-        <div className="aboutimg2">
-          <h1 className=" aboutt">
-            <Link to={"/recipes"}>Recipes</Link>
-          </h1>
-          <div className="color-overlay"></div>
-        </div>
+        <Link to={"/sharing"}>
+          <div className="aboutimg1">
+            <h1 className=" aboutt">Sharing</h1>
+            <div className="color-overlay"></div>
+          </div>
+        </Link>
+        <Link to={"/recipes"}>
+          <div className="aboutimg2">
+            <h1 className=" aboutt">Recipes</h1>
+            <div className="color-overlay"></div>
+          </div>
+        </Link>
       </div>
       <div>
         <br />
