40ac119d8d75cf8d019174b5bc7dc8c78b437556
Abdullah Al Shabk <89540228+Abdushabk@users.noreply.github.com>
2022-01-20 00:52:52 +0100

    styling,smoother-animation,update Logo & Easter-egg

--- src/content/pages/MyShares.js
+++ src/content/pages/MyShares.js
@@ -195,7 +195,7 @@ const MyShares = ({ APPDATA }) => {
         New Messages:
         {shareMessages.map((i) =>
           !i[2] ? (
-            <div key={k++} style={{ backgroundColor: "lightblue", borderRadius:"25px 20px -2px 30px",
+            <div key={k++} style={{ backgroundColor: "lightblue", borderRadius:"25px 20px 0px 30px",
             padding: "10px", marginTop:"10px" }}>
               <span style={{ fontSize: "1.2rem" }}>{i[1]}</span> -{" "}
               <i>
